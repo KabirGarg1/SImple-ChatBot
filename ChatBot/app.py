6002ab3881ef2d21a2054ef24ba8d344a4f11057
KabirGarg1 <gargkabir513@gmail.com>
2024-09-09 23:32:33 +0530

    Initial commit

---
+++ ChatBot/app.py
@@ -0,0 +1,27 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt Template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+
+st.title('Langchain Demo With OPENAI API')
+input_text=st.text_input("Search the topic u want")
+
